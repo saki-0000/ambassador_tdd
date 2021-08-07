b12975dc176cbf257c2996b789d0d9049b7f2dce
saki-0000 <saki_27_maruko@yahoo.co.jp>
2021-08-07 19:35:47 +0900

    レジスターのルーティングを通した。

---
+++ laravel-ambassador/tests/Feature/AdminTest.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class AdminTest extends TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function test_レジスター()
+    {
+        $response = $this->post('/admin/regester');
+
+        dump($response);
+
+        $response->assertJson(['test']);
+        $response->assertStatus(200);
+    }
+}
