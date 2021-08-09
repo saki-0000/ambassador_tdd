0f96db0e8deaeec2df757e70ea224931e0c1fc75
saki-0000 <saki_27_maruko@yahoo.co.jp>
2021-08-09 10:20:06 +0900

    リファクタリングとTODO更新（→ログイン）

--- TODO.md
+++ TODO.md
@@ -5,8 +5,9 @@
 - post register
   - ~~testという文字列をとりあえず返す。~~
   - ~~postリクエストとバリデーション~~
-  - ユーザーを作成する（レジスター）
+  - ~~ユーザーを作成する（レジスター）~~
 - post login
+  - ログインできる
 - post logout
 - get user
 
--- laravel-ambassador/app/Http/Controllers/AuthController.php
+++ laravel-ambassador/app/Http/Controllers/AuthController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\RegisterRequest;
 use App\Models\User;
+use Symfony\Component\HttpFoundation\Response;
 
 class AuthController extends Controller
 {
@@ -15,10 +16,11 @@ class AuthController extends Controller
      */
     public function register(RegisterRequest $request)
     {
-        return User::create($request->only([
+        $user = User::create($request->only([
             'first_name', 'last_name', 'email'
         ]) + [
             'password' => 'a', 'is_admin' => 1
         ]);
+        return response($user, Response::HTTP_CREATED);
     }
 }
